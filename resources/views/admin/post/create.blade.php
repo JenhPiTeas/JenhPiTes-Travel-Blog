4ed286b63b66aae1fa5bad90b55f0ba3b8192f3d
kak_reak <timberr.4t@gmail.com>
2023-04-20 22:27:15 +0700

    bla bla bla

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -48,7 +48,7 @@
 
                     <div class="mb-3">
                         <label for="" class="form-label">Youtube Iframe Link</label>
-                        <input type="text" name="yt_iframe" class="form-control">
+                        <input type="file" name="yt_iframe" class="form-control">
                     </div>
 
                     <h4>SEO Tags</h4>
