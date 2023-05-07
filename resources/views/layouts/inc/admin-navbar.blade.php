9d4a2c99afc2b8fd827db098871bc5877cb160eb
kak_reak <timberr.4t@gmail.com>
2023-05-07 23:38:03 +0700

    admin logout

--- resources/views/layouts/inc/admin-navbar.blade.php
+++ resources/views/layouts/inc/admin-navbar.blade.php
@@ -18,7 +18,13 @@
                     <li>
                         <hr class="dropdown-divider"/>
                     </li>
-                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                    <li><a class="dropdown-item" href=" {{ route('logout') }} " onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                    </li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </ul>
             </li>
         </ul>
